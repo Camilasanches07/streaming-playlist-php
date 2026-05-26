a15fe9af2a2d690cad6eff7b9474b9ad52f243c0
Camila Sanches <camilasanchesferreira@hotmail.com>
2026-05-26 20:59:05 -0300

    atualizacao reproduzivel

--- index.php
+++ index.php
@@ -2,4 +2,16 @@
 
 declare(strict_types=1);
 
-echo "Projeto App de Streaming";
+require_once __DIR__ . '/Reproduzivel.php';
+require_once __DIR__ . '/Midia.php';
+require_once __DIR__ . '/VideoCurto.php';
+
+$videoCurto = new VideoCurto(
+    titulo: 'Dica rápida de PHP',
+    criador: 'Camila',
+    duracao: 60,
+    urlVideo: 'https://exemplo.com/video',
+    urlThumbnail: 'https://exemplo.com/thumb.jpg'
+);
+
+echo $videoCurto->reproduzir() . PHP_EOL;
