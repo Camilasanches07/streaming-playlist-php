04f8a58d6ab44b56898aae061306f86ba01ea462
luisazevedo-coder <luis.azevedo@alunos.unis.edu.br>
2026-05-26 20:24:01 -0300

    Refactor constructor properties to multiline format

--- VideoCurto.php
+++ VideoCurto.php
@@ -6,11 +6,11 @@ class VideoCurto {
     private const LIMITE_DURACAO_SEGUNDOS = 90;
 
     public function __construct(
-        private int $id,
-        private string $titulo,
-        private string $urlVideo,
-        private int $duracao,
-        private string $urlThumbnail = '',
+        private int $id;
+        private string $titulo;
+        private string $urlVideo;
+        private int $duracao;
+        private string $urlThumbnail = '';
         ) 
 {
 ?>
