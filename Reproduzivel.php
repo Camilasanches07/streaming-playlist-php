f72900d25ef752381a4e061e4dd981b28c9eeb15
Pedro Henrique <pedro.santos11@alunos.unis.edu.br>
2026-05-26 20:53:19 -0300

    corrigi Reproduzivel

--- Reproduzivel.php
+++ Reproduzivel.php
@@ -1,28 +1,8 @@
 <?php
+
 declare(strict_types=1);
 
 interface Reproduzivel
-    {
-        public function reproduzir(): string;
-    }
-abstract class Midia implements Reproduzivel
-    {
-        public function __construct(
-            private string $titulo,
-            private string $criador,
-            private int $duracao
-        ) {
-        }
-        public function getTitulo(): string
-        {
-            return $this->titulo;
-        }
-        public function getCriador(): string
-        {
-            return $this->criador;
-        }
-        public function getDuracao(): int
-        {
-            return $this->duracao;
-        }
-    }
+{
+    public function reproduzir(): string;
+}
