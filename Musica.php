c2b02cf8e2e335724ca3d7d9de44cf58bae57a22
pedrosantos11-ads <pedro.santos11@alunos.unis.edu.br>
2026-05-26 21:09:14 -0300

    Corrige Musica e Podcast: private, implements Reproduzivel e require Reproduzivel

--- Musica.php
+++ Musica.php
@@ -1,20 +1,27 @@
 <?php
+
 declare(strict_types=1);
 
-require_once 'Midia.php'; 
+require_once 'Midia.php';
+require_once 'Reproduzivel.php';
 
-class Musica extends Midia 
+class Musica extends Midia implements Reproduzivel
 {
     public function __construct(
-        string $titulo, 
-        string $artista, 
-        int $duracao, 
-        public string $dataPublicacao
+        string $titulo,
+        string $artista,
+        int $duracao,
+        private string $dataPublicacao
     ) {
         parent::__construct($titulo, $artista, $duracao);
     }
 
-    public function reproduzir(): string 
+    public function getDataPublicacao(): string
+    {
+        return $this->dataPublicacao;
+    }
+
+    public function reproduzir(): string
     {
         return "Tocando a música: '" . $this->getTitulo() . "' de " . $this->getCriador() . ".";
     }
